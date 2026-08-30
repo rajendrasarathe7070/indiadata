e6606ec5cfa197375270a324b1d5e0be92bb32f7
Rajendra Sarathe <143543512+rajendrasarathe7070@users.noreply.github.com>
2026-08-30 22:54:34 +0530

    'ifsc_codes' to 'IFSC'

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
+@app.get("/")
+def ping():
+    return {"status": "success", "message": "Advanced FastAPI Server is active!" ,"by-": "Rajendra Sarathe" }
+
 @app.get("/ping")
 def ping():
     return {"status": "success", "message": "Advanced FastAPI Server is active!"}
@@ -44,7 +48,7 @@ def get_pincode_data(code: str):
 def get_ifsc_data(code: str):
     try:
         ifsc_code = code.upper().strip()
-        response = supabase.table("ifsc_codes").select("*").eq("ifsc", ifsc_code).execute()
+        response = supabase.table("IFSC").select("*").eq("ifsc", ifsc_code).execute()
         
         # Yahan array empty hone par crash nahi hoga, gracefully 404 dega
         if not response.data or len(response.data) == 0:
@@ -73,7 +77,7 @@ def get_districts_by_state(state: str = Query(..., description="State name e.g. 
 @app.get("/api/v1/banks/unique")
 def get_unique_banks():
     try:
-        response = supabase.table("ifsc_codes").select("BANK").execute()
+        response = supabase.table("IFSC").select("BANK").execute()
         if not response.data or len(response.data) == 0:
             return {"success": True, "banks": []}
         unique_banks = list(set([row['BANK'] for row in response.data if row.get('BANK')]))
@@ -90,6 +94,6 @@ def search_bank_branch(
         bank_name = bank.strip()
         city_name = city.strip()
         # ilike lagane se users lowercase me bhi search karenge to sahi data milega
-        response = supabase.table("ifsc_codes").select("BRANCH,ifsc,ADDRESS").ilike("BANK", bank_name).ilike("CITY", city_name).execute()
+        response = supabase.table("IFSC").select("BRANCH,IFSC,ADDRESS").ilike("BANK", bank_name).ilike("CITY", city_name).execute()
         return {"success": True, "count": len(response.data), "branches": response.data}
     except Exception as e: raise HTTPException(status_code=500, detail="Error searching branches")
